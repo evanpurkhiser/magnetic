283aa426d74b11282a0d8363effdbab680526019
Evan Purkhiser <evanpurkhiser@gmail.com>
2018-01-21 23:59:05 -0800

    Many updates that I should be commiting
     - Switches to use coco pods
     - Implement 1password
     - Add logo / description text / icon
     - Style configuration form

--- Magnetic/ViewController.swift
+++ Magnetic/ViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import OnePasswordExtension
 
 func getTransmissionProtectionSpace(host: String?) -> URLProtectionSpace {
     return URLProtectionSpace(
@@ -9,10 +10,41 @@ func getTransmissionProtectionSpace(host: String?) -> URLProtectionSpace {
         authenticationMethod: NSURLAuthenticationMethodHTTPBasic)
 }
 
+class SettingsField: UITextField {
+    var padding = UIEdgeInsetsMake(2, 10, 0, 10)
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.borderStyle = UITextBorderStyle.none
+        self.layer.backgroundColor = UIColor(red:0.93, green:0.94, blue:0.94, alpha:1.0).cgColor
+        self.layer.cornerRadius = 4
+    }
+
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, self.padding)
+    }
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return self.textRect(forBounds: bounds)
+    }
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        var rect = self.textRect(forBounds: bounds)
+        rect.size.width -= 16
+        return rect
+    }
+    override func clearButtonRect(forBounds bounds: CGRect) -> CGRect {
+        var rect = super.clearButtonRect(forBounds: bounds)
+        rect.origin.x -= self.padding.right - 6
+        return rect
+    }
+}
+
 class ViewController: UIViewController {
-    @IBOutlet weak var hostname: UITextField!
-    @IBOutlet weak var username: UITextField!
-    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var hostname: SettingsField!
+    @IBOutlet weak var username: SettingsField!
+    @IBOutlet weak var password: SettingsField!
+    @IBOutlet weak var passButton: UIButton!
+
+    @IBOutlet weak var settingsOffset: NSLayoutConstraint!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,18 +54,44 @@ class ViewController: UIViewController {
         let urlProtectionSpace = getTransmissionProtectionSpace(host: host)
         let creds = URLCredentialStorage.shared.defaultCredential(for: urlProtectionSpace)
 
-        self.password.isSecureTextEntry = true
         self.hostname.text = host
         self.username.text = creds?.user
         self.password.text = creds?.password
 
+        self.password.isSecureTextEntry = true
+
+        // TODO: Hide if extension is missing
+        let border = CALayer()
+        border.backgroundColor = UIColor.white.cgColor
+        border.frame = CGRect(x: -2,y: 0, width: 2, height: self.passButton.frame.height)
+        self.passButton.layer.addSublayer(border)
+        self.passButton.imageEdgeInsets = UIEdgeInsetsMake(10, 10, 10, 10)
+        self.password.padding = UIEdgeInsetsMake(2, 10, 0, 10 + self.passButton.frame.width)
+
         self.view.addGestureRecognizer(UITapGestureRecognizer(
             target: self.view,
             action: #selector(self.view.endEditing)))
+
+        NotificationCenter.default.addObserver(self,
+            selector: #selector(ViewController.keyboardWillChange),
+            name: NSNotification.Name.UIKeyboardWillShow,
+            object: nil)
+
+        NotificationCenter.default.addObserver(self,
+            selector: #selector(ViewController.keyboardWillChange),
+            name: NSNotification.Name.UIKeyboardWillHide,
+            object: nil)
     }
 
-    func torrentWasAdded(resp: TransmissionResponse) {
-        print(resp)
+    @objc func keyboardWillChange(notification: NSNotification) {
+        guard let keyboardSize = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+            return
+        }
+
+        let mult: CGFloat = notification.name == NSNotification.Name.UIKeyboardWillShow ? 1 : -1;
+        self.settingsOffset.constant += mult * (keyboardSize.height - 60)
+
+        UIView.animate(withDuration: 0.1) { self.view.layoutIfNeeded() }
     }
 
     @IBAction func updateHostname() {
@@ -53,4 +111,26 @@ class ViewController: UIViewController {
             for: protectionSpace)
     }
 
+    @IBAction func passwordAutofill(_ sender: UIButton) {
+        let pw = OnePasswordExtension.shared()
+
+        var url = URLComponents()
+        url.host = self.hostname.text
+        url.scheme = "https"
+
+        let urlString = self.hostname.text != "" ? url.string! : "transmission"
+
+        pw.findLogin(forURLString: urlString, for: self, sender: sender) { (loginDictionary, error) in
+            guard let loginDictionary = loginDictionary else { return }
+
+            print(loginDictionary)
+
+            self.username.text = loginDictionary[AppExtensionUsernameKey] as? String
+            self.password.text = loginDictionary[AppExtensionPasswordKey] as? String
+        }
+    }
+
+    func torrentWasAdded(resp: TransmissionResponse) {
+        print(resp)
+    }
 }
---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '9.0'
+
+target 'Magnetic' do
+  use_frameworks!
+
+  pod '1PasswordExtension', '~> 1.8.5'
+end
